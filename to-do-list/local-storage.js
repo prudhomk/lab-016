8f34ae6b03e8ca1f53e36c4c89173d0c8ebb1321
Austin Summerlin <summerlin.austin@gmail.com>
2021-04-14 14:15:16 -0700

    local-storage-utils

--- to-do-list/local-storage.js
+++ to-do-list/local-storage.js
@@ -1,12 +1,50 @@
 const USER = 'USER';
+const LOGGEDIN = 'LOGGEDIN';
 
-
-function createUser(username, password) {
-    const user = { username, password, todos:[] };
-
-    const stringyUser = JSON.stringify(user);
-    
-    localStorage.setItem(USER, stringyUser);
-
-
+export function createUser(username, password) {
+    const user = {
+        username,
+        password,
+        todos: []
+    };
+    updateUser(user);
 }
+
+export function updateUser(user) {
+    const stringyUser = JSON.stringify(USER);
+    user = JSON.parse(stringyUser);
+
+    return user;
+}
+
+export function getUser(user) {
+    const stringyUser = localStorage.getItem(USER);
+
+    user = JSON.parse(stringyUser);
+
+    return user;
+}
+
+export function checkUser(username) {
+    const storedUser = getUser();
+    if (!storedUser) return false;
+
+    return username === storedUser.username;
+}
+
+export function correctLogin(username, password) {
+    const userInLocalStorage = getUser();
+
+    if (!checkUser(username) || userInLocalStorage.password !== password) return false;
+
+    return true;
+}
+
+export function login(username) {
+    localStorage.setItem(LOGGEDIN, username);
+}
+
+export function logout() {
+    localStorage.setItem(LOGGEDIN, '');
+}
+
